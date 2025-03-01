de89d88d957c6ccd9a18b3c9b4218dd2ca9bc517
Solayman RAZINE <solayman.razine@accenta.ai>
2025-03-01 17:04:27 +0100

    rajout de plein de pages par solayman

--- js/main.js
+++ js/main.js
@@ -36,25 +36,6 @@ Version:	1.1
     "use strict";
      $(document).on('ready', function() {
 	
-        jQuery(window).on('scroll', function() {
-			if ($(this).scrollTop() > 200) {
-				$('#header .header-inner').addClass("sticky");
-			} else {
-				$('#header .header-inner').removeClass("sticky");
-			}
-		});
-		
-		/*====================================
-			Sticky Header JS
-		======================================*/ 
-		jQuery(window).on('scroll', function() {
-			if ($(this).scrollTop() > 100) {
-				$('.header').addClass("sticky");
-			} else {
-				$('.header').removeClass("sticky");
-			}
-		});
-		
 		$('.pro-features .get-pro').on( "click", function(){
 			$('.pro-features').toggleClass('active');
 		});
